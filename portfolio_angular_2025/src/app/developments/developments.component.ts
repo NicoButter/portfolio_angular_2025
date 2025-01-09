b80db7509d3d3d6c20f890b7ec2680781dcfb541
Nicolás Butterfield <nicobutter@gmail.com>
2025-01-08 21:59:35 -0300

    header updates

---
+++ portfolio_angular_2025/src/app/developments/developments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-developments',
+  standalone: true,
+  imports: [],
+  templateUrl: './developments.component.html',
+  styleUrl: './developments.component.css'
+})
+export class DevelopmentsComponent {
+
+}
