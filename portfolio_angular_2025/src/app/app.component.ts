40b3448a700605c47237fc446d6180d69fb4870f
Nicolás Butterfield <nicobutter@gmail.com>
2025-01-10 20:07:42 -0300

    aplicando mejoras en skills

--- portfolio_angular_2025/src/app/app.component.ts
+++ portfolio_angular_2025/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, AboutMeComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- portfolio_angular_2025/src/app/skills/skills.component.ts
+++ portfolio_angular_2025/src/app/skills/skills.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [NgCircleProgressModule],
   templateUrl: './skills.component.html',
   styleUrl: './skills.component.css'
 })
