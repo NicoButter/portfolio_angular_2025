ef9f2f2b58887b0fbbecc661369107e05f6b84cd
Nicolás Butterfield <nicobutter@gmail.com>
2025-01-11 16:23:07 -0300

    ajuste en my skills

--- portfolio_angular_2025/src/app/skills/skills.component.ts
+++ portfolio_angular_2025/src/app/skills/skills.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 
-
 @Component({
   selector: 'app-skills',
   standalone: true,
   imports: [NgCircleProgressModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
-
-}
+export class SkillsComponent {}
--- portfolio_angular_2025/src/main.ts
+++ portfolio_angular_2025/src/main.ts
@@ -1,6 +1,25 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    importProvidersFrom(
+      NgCircleProgressModule.forRoot({
+        radius: 80,
+        outerStrokeWidth: 8,
+        innerStrokeWidth: 5,
+        outerStrokeColor: "#FFBF00",
+        innerStrokeColor: "#808080",
+        animation: true,
+        animationDuration: 750,
+        showImage: true,
+        showUnits: false,
+        showTitle: false,
+      }),
+    ),
+    ...appConfig.providers, // Aquí es donde se "extiende" appConfig
+  ]
+}).catch((err) => console.error(err));
