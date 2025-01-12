89698fac22a32b3c714c6a61fe696a1ef1c0cfb7
Nicolás Butterfield <nicobutter@gmail.com>
2025-01-11 23:24:18 -0300

    footer and more  updates

--- portfolio_angular_2025/src/app/app.component.ts
+++ portfolio_angular_2025/src/app/app.component.ts
@@ -4,11 +4,19 @@ import { HeaderComponent } from './header/header.component';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { SkillsComponent } from './skills/skills.component';
 import { DevelopmentsComponent } from './developments/developments.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, AboutMeComponent, SkillsComponent, DevelopmentsComponent],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent, 
+    AboutMeComponent, 
+    SkillsComponent, 
+    DevelopmentsComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
